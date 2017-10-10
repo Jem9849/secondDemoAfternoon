7b03c635855cd12deb476ff25b05750b23ed7686
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-10-10 12:23:07 -0600

    Edited a ton of stuff but forgot to make a repository, so far I've made a new project and added color button that changes its color and its background color however it currently doesn't work for some reason.

---
+++ secondDemoAfternoon/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  secondDemoAfternoon
+//
+//  Created by Saysanasy, Jeremy on 10/10/17.
+//  Copyright © 2017 Saysanasy, Jeremy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var count = 0
+    // Variable for button color changer.
+    
+    
+    // Made a link to button in storyboard for output.
+    @IBOutlet weak var firstButton: UIButton!
+    
+    // Made an action link for button which changes color based on even and odds from when you press it.
+    @IBAction func firstMethod(_ sender: UIButton)
+    {
+        
+        // This is an if block that changes color of button based on even and odds.
+        if (count % 2 == 0)
+        {
+            firstButton.backgroundColor = .green
+            count += 1
+        }
+        else if (count % 2 == 1)
+        {
+            firstButton.backgroundColor = .blue
+            count += 1
+        }
+        // Then I call createRandomColor() which changes the backgroundColor.
+        view.backgroundColor = createRandomColor()
+    }
+    
+    // This here is where I make the color changer random for the background.
+    private func createRandomColor() -> UIColor
+    {
+        // These all make new values to be used for random colors.
+        let newColor :UIColor
+        let redValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let greenValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let blueValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+            newColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha:CGFloat(1.0))
+        return newColor // I return newColor.
+    }
+
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
