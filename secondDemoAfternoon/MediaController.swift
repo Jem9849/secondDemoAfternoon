f8e19898a5ce943e1ebfa4f77a0afe1d847980a4
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-10-18 13:29:05 -0600

    Added image and music files and added a change picture button and also edited the controller to be called mediacontroller.

---
+++ secondDemoAfternoon/MediaController.swift
@@ -0,0 +1,99 @@
+//
+//  MediaController.swift
+//  secondDemoAfternoon
+//
+//  Created by Saysanasy, Jeremy on 10/18/17.
+//  Copyright © 2017 Saysanasy, Jeremy. All rights reserved.
+//
+
+import UIKit
+
+public class MediaController: UIViewController
+{
+    private lazy var color : ColorTools = ColorTools()
+    private var imageCounter : Int = 0
+    
+    @IBOutlet weak var changePicture: UIButton!
+    
+    @IBOutlet weak var playPause: UIButton!
+    
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func imageButtonClick(_ sender: UIButton) -> Void
+    {
+        changeImage()
+        view.backgroundColor = color.createRandomColor()
+    }
+    
+    private func changeImage() -> Void
+    {
+        if (imageCounter > 5)
+        {
+            imageCounter = 0
+        }
+        
+        if (imageCounter == 0)
+        {
+            imageView.image = UIImage(named: "o")
+        }
+        else if (imageCounter == 1)
+        {
+            imageView.image = UIImage(named: "ok")
+        }
+        else if (imageCounter == 2)
+        {
+            imageView.image = UIImage (named: "Cats" )
+        }
+        else if (imageCounter == 3)
+        {
+            imageView.image = UIImage (named: "boo")
+        }
+        else if (imageCounter == 4)
+        {
+            imageView.image = UIImage(named: "black")
+        }
+        else
+        {
+            imageView.image = UIImage(named: "WAT")
+        }
+        imageCounter += 1
+    }
+    
+    @IBAction func playStopClick(_ sender: UIButton)
+    {
+        
+    }
+    
+    
+    @IBAction func volumeSliderMove(_ sender: UISlider)
+    {
+        
+    }
+    
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    public override func didReceiveMemoryWarning() -> Void
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
