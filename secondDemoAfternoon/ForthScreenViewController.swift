d6766e852b2e1678e979c4699fdeaa9a1aff1a2b
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-10-18 11:47:14 -0600

    Added a new screen view and added buttons, sliders, and a image view.

---
+++ secondDemoAfternoon/ForthScreenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ForthScreenViewController.swift
+//  secondDemoAfternoon
+//
+//  Created by Saysanasy, Jeremy on 10/18/17.
+//  Copyright © 2017 Saysanasy, Jeremy. All rights reserved.
+//
+
+import UIKit
+
+public class ForthScreenViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    public override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
